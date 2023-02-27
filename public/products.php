f2e2581975a0976bc554b14010cc8cfe989d8fd7
nadaissa <nada-issa_student2023@wilder.school>
2023-02-27 11:36:54 +0100

    initial solution products twig01

---
+++ public/products.php
@@ -0,0 +1,11 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig',
+compact('products')
+// ['products' => $products]
+);
+// var_dump($products);
